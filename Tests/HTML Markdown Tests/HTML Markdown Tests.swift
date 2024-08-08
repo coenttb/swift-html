55a821c95337bd2ff927f7b202328b4e771f3bc2
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.com>
2024-08-08 12:02:10 +0200

    fix

--- Tests/HTML Markdown Tests/HTML Markdown Tests.swift
+++ Tests/HTML Markdown Tests/HTML Markdown Tests.swift
@@ -13,36 +13,26 @@ import HTML_Markdown
 func markdown(){
     
     
-    let markdown = createMarkdown {
-        "1"
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
-        ["2", "3"]
+   print(
+    createMarkdown {
+        if true {
+            "## de_volgende_artikelen_van_deze_titel_behalve_artikel_5_gelden_niet_voor_de_rechtspersoon"
+        }
+        if true {
+            "### met_uitzondering_van_artikel_5_gelden_de_volgende_artikelen_van_deze_titel_niet_voor_de_rechtspersoon"
+        }
+        if true {
+            "# overeenkomstige_toepassing_van_de_volgende_artikelen_van_deze_titel_is_geoorloofd"
+        }
     }
-    
-    
-    print(markdown)
+   )
     
 }
+
+
+
+
+
 func createMarkdown(@MarkdownBuilder _ content: () -> String) -> String {
     content()
 }
