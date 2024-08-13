13c606f7fad87146f27170e98f0f655c16dbbf35
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.com>
2024-08-13 10:30:12 +0200

    wip

--- Sources/HTML/HTMLHelpers/Flex.swift
+++ Sources/HTML/HTMLHelpers/Flex.swift
@@ -30,3 +30,64 @@ extension HTML {
             .inlineStyle("flex-basis", basis, media: media)
     }
 }
+
+extension HTML {
+    public func flexContainer(
+        direction: Flex.Direction? = nil,
+        wrap: Flex.Wrap? = nil,
+        justification: String? = nil,
+        itemAlignment: String? = nil,
+        rowGap: String? = nil,
+        columnGap: String? = nil,
+        media: MediaQuery? = nil
+    ) -> some HTML {
+        self.flexContainer(
+            direction: direction?.rawValue,
+            wrap: wrap?.rawValue,
+            justification: justification,
+            itemAlignment: itemAlignment,
+            rowGap: rowGap,
+            columnGap: columnGap,
+            media: media
+        )
+    }
+    
+    public func flexItem(
+        grow: Flex.Grow? = nil,
+        shrink: Flex.Shrink? = nil,
+        basis: Flex.Basis? = nil,
+        media: MediaQuery? = nil
+    ) -> some HTML {
+        self
+            .inlineStyle("flex-grow", grow.map(String.init), media: media)
+            .inlineStyle("flex-shrink", shrink.map(String.init), media: media)
+            .inlineStyle("flex-basis", basis?.rawValue, media: media)
+    }
+}
+
+public struct Flex {    
+    public typealias Grow = Int
+    public typealias Shrink = Int
+    
+    public enum Basis: String {
+        case flexBasis = "flex-basis"
+        case auto
+        case content
+        case fitContent = "fit-content"
+        case maxContent = "max-content"
+        case minContent = "min-content"
+    }
+    public enum Direction: String {
+        case flexDirection = "flex-direction"
+        case column
+        case columnReverse = "column-reverse"
+        case row
+        case rowReverse = "row-reverse"
+    }
+    public enum Wrap: String {
+        case flexWrap = "flex-wrap"
+        case nowrap
+        case wrap
+        case wrapReverse = "wrap-reverse"
+    }
+}
