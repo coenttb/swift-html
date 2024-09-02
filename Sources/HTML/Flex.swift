6b681687ffa0af6be71aa197accd9c20bdafe0ee
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-09-02 20:16:48 +0200

    wip

--- Sources/HTML/Flex.swift
+++ Sources/HTML/Flex.swift
@@ -2,25 +2,25 @@ import CSS
 import HTMLCore
 
 extension HTML {
-    @_disfavoredOverload
-    public func flexContainer(
-        direction: String? = nil,
-        wrap: String? = nil,
-        justification: String? = nil,
-        itemAlignment: String? = nil,
-        rowGap: String? = nil,
-        columnGap: String? = nil,
-        media: MediaQuery? = nil
-    ) -> some HTML {
-        self
-            .display(.flex, media: media)
-            .inlineStyle("flex-direction", direction, media: media)
-            .inlineStyle("flex-wrap", wrap, media: media)
-            .inlineStyle("justify-content", justification, media: media)
-            .inlineStyle("align-items", itemAlignment, media: media)
-            .inlineStyle("row-gap", rowGap, media: media)
-            .inlineStyle("column-gap", columnGap, media: media)
-    }
+//    @_disfavoredOverload
+//    public func flexContainer(
+//        direction: String? = nil,
+//        wrap: String? = nil,
+//        justification: String? = nil,
+//        itemAlignment: String? = nil,
+//        rowGap: String? = nil,
+//        columnGap: String? = nil,
+//        media: MediaQuery? = nil
+//    ) -> some HTML {
+//        self
+//            .display(.flex, media: media)
+//            .inlineStyle("flex-direction", direction, media: media)
+//            .inlineStyle("flex-wrap", wrap, media: media)
+//            .inlineStyle("justify-content", justification, media: media)
+//            .inlineStyle("align-items", itemAlignment, media: media)
+//            .inlineStyle("row-gap", rowGap, media: media)
+//            .inlineStyle("column-gap", columnGap, media: media)
+//    }
     
     public func flexItem(
         grow: String? = nil,
