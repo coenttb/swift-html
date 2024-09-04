594de010c7f6962a97c3da509738488cb96f575a
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-09-04 08:25:03 +0200

    Divider color

--- Sources/HTML/Divider.swift
+++ Sources/HTML/Divider.swift
@@ -3,7 +3,7 @@ public struct Divider: HTML {
   public var body: some HTML {
     hr()
       .border(.none)
-      .border(.top(width: 1.px, style: .solid, color: .gray800.dark(.gray300)))
+      .border(.top(width: 1.px, style: .solid, color: .gray400.dark(.gray300)))
       .margin(vertical: 0, horizontal: 30.percent)
   }
 }
