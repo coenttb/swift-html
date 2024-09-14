ce12a981de9376790b9e1c3230af0d7a37de2725
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-09-14 14:16:47 +0200

    wip

--- Sources/HTML Markdown/HTMLMarkdown.swift
+++ Sources/HTML Markdown/HTMLMarkdown.swift
@@ -452,20 +452,6 @@ extension HTMLBuilder {
     }
 }
 
-public struct AnyHTML: HTML {
-    let base: any HTML
-    init(_ base: any HTML) {
-        self.base = base
-    }
-    public static func _render(_ html: AnyHTML, into printer: inout HTMLPrinter) {
-        func render<T: HTML>(_ html: T) {
-            T._render(html, into: &printer)
-        }
-        render(html.base)
-    }
-    public var body: Never { fatalError() }
-}
-
 private struct BlockQuoteStyle {
     var backgroundColor: HTMLColor
     var borderColor: HTMLColor
