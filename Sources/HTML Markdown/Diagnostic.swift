54bb8d18bbd1930a9efee3e1706c2b6bb2e4c0ca
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-09-14 13:18:05 +0200

    wip

--- Sources/HTML Markdown/Diagnostic.swift
+++ Sources/HTML Markdown/Diagnostic.swift
@@ -11,49 +11,49 @@ public struct DiagnosticLevel: Sendable {
     
     public static let error = Self(
         icon: .error,
-        iconColor: .init(light: .hex("#CA0900"), dark: .hex("#ED2239")),
-        highlightColor: .init(light: .hex("#EFE9F2"), dark: .hex("#3A2C30")),
-        underlineColor: .init(light: .hex("#E31315"), dark: .hex("#E21415")),
-        backgroundColor: .init(light: .hex("#FFC0C0"), dark: .hex("#863432")),
-        detailBackgroundColor: .init(light: .hex("#EFDCDC"), dark: .hex("#402E2B")),
-        buttonBackgroundColor: .init(light: .hex("#B7AAA9"), dark: .hex("#302221"))
+        iconColor: .init(light: .hex("CA0900"), dark: .hex("ED2239")),
+        highlightColor: .init(light: .hex("EFE9F2"), dark: .hex("3A2C30")),
+        underlineColor: .init(light: .hex("E31315"), dark: .hex("E21415")),
+        backgroundColor: .init(light: .hex("FFC0C0"), dark: .hex("863432")),
+        detailBackgroundColor: .init(light: .hex("EFDCDC"), dark: .hex("402E2B")),
+        buttonBackgroundColor: .init(light: .hex("B7AAA9"), dark: .hex("302221"))
     )
     
     public static let issue = Self(
         icon: .failure,
-        iconColor: .init(light: .hex("#CA0900"), dark: .hex("#ED2239")),
-        highlightColor: .init(light: .hex("#EFE9F2"), dark: .hex("#3A2C30")),
-        backgroundColor: .init(light: .hex("#FFC0C0"), dark: .hex("#863432")),
-        detailBackgroundColor: .init(light: .hex("#EFDCDC"), dark: .hex("#402E2B")),
-        buttonBackgroundColor: .init(light: .hex("#B7AAA9"), dark: .hex("#302221"))
+        iconColor: .init(light: .hex("CA0900"), dark: .hex("ED2239")),
+        highlightColor: .init(light: .hex("EFE9F2"), dark: .hex("3A2C30")),
+        backgroundColor: .init(light: .hex("FFC0C0"), dark: .hex("863432")),
+        detailBackgroundColor: .init(light: .hex("EFDCDC"), dark: .hex("402E2B")),
+        buttonBackgroundColor: .init(light: .hex("B7AAA9"), dark: .hex("302221"))
     )
     
     public static let knownIssue = Self(
         icon: .failure,
-        iconColor: .init(light: .hex("#8E8E93"), dark: .hex("#98989D")),
-        highlightColor: .init(light: .hex("#F4F4F4"), dark: .hex("#333439")),
-        backgroundColor: .init(light: .hex("#CECED1"), dark: .hex("#5F5F61")),
-        detailBackgroundColor: .init(light: .hex("#E0E0E0"), dark: .hex("#373635")),
-        buttonBackgroundColor: .init(light: .hex("#A8A8A8"), dark: .hex("#292828"))
+        iconColor: .init(light: .hex("8E8E93"), dark: .hex("98989D")),
+        highlightColor: .init(light: .hex("F4F4F4"), dark: .hex("333439")),
+        backgroundColor: .init(light: .hex("CECED1"), dark: .hex("5F5F61")),
+        detailBackgroundColor: .init(light: .hex("E0E0E0"), dark: .hex("373635")),
+        buttonBackgroundColor: .init(light: .hex("A8A8A8"), dark: .hex("292828"))
     )
     
     public static let runtimeWarning = Self(
         icon: .warning,
-        iconColor: .init(light: .hex("#A156D5"), dark: .hex("#A849E8")),
-        highlightColor: .init(light: .hex("#E9EAFD"), dark: .hex("#3A3447")),
-        backgroundColor: .init(light: .hex("#D9B8F3"), dark: .hex("#714088")),
-        detailBackgroundColor: .init(light: .hex("#EBE1F2"), dark: .hex("#3A303C")),
-        buttonBackgroundColor: .init(light: .hex("#ACA3AF"), dark: .hex("#2B252E"))
+        iconColor: .init(light: .hex("A156D5"), dark: .hex("A849E8")),
+        highlightColor: .init(light: .hex("E9EAFD"), dark: .hex("3A3447")),
+        backgroundColor: .init(light: .hex("D9B8F3"), dark: .hex("714088")),
+        detailBackgroundColor: .init(light: .hex("EBE1F2"), dark: .hex("3A303C")),
+        buttonBackgroundColor: .init(light: .hex("ACA3AF"), dark: .hex("2B252E"))
     )
     
     public static let warning = Self(
         icon: .warning,
-        iconColor: .init(light: .hex("#FFBA00"), dark: .hex("#FFC502")),
-        highlightColor: .init(light: .hex("#FFFAEA"), dark: .hex("#3F3A30")),
-        underlineColor: .init(light: .hex("#FEC300")),
-        backgroundColor: .init(light: .hex("#FFEAAD"), dark: .hex("#8F7723")),
-        detailBackgroundColor: .init(light: .hex("#EFE9D6"), dark: .hex("#413B29")),
-        buttonBackgroundColor: .init(light: .hex("#B4AEA0"), dark: .hex("#312C1E"))
+        iconColor: .init(light: .hex("FFBA00"), dark: .hex("FFC502")),
+        highlightColor: .init(light: .hex("FFFAEA"), dark: .hex("3F3A30")),
+        underlineColor: .init(light: .hex("FEC300")),
+        backgroundColor: .init(light: .hex("FFEAAD"), dark: .hex("8F7723")),
+        detailBackgroundColor: .init(light: .hex("EFE9D6"), dark: .hex("413B29")),
+        buttonBackgroundColor: .init(light: .hex("B4AEA0"), dark: .hex("312C1E"))
     )
 }
 
@@ -100,8 +100,8 @@ public struct Diagnostic<Message: HTML>: HTML {
                 .padding(8.px)
             }
             .border(.radius(8.px))
-            .border(width: 0.5.px, style: .solid, color: .init(light: .hex("#\(level.backgroundColor.light)44")))
-            .border(width: 0.5.px, style: .solid, color: .init(light: .hex("#\(level.backgroundColor.dark!)44")), media: .dark)
+            .border(width: 0.5.px, style: .solid, color: .init(light: .hex("\(level.backgroundColor.light)44")))
+            .border(width: 0.5.px, style: .solid, color: .init(light: .hex("\(level.backgroundColor.dark!)44")), media: .dark)
             .overflow(.hidden)
         }
         .inlineStyle(
--- Sources/HTML Markdown/HTMLMarkdown.swift
+++ Sources/HTML Markdown/HTMLMarkdown.swift
@@ -473,20 +473,20 @@ private struct BlockQuoteStyle {
     init(blockName: String) {
         switch blockName {
         case "Warning", "Correction":
-            self.backgroundColor = HTMLColor(light: .hex("#FDF2F4"), dark: .hex("#2E0402"))
-            self.borderColor = HTMLColor(light: .hex("#D02C1E"), dark: .hex("#EB4642"))
+            self.backgroundColor = HTMLColor(light: .hex("FDF2F4"), dark: .hex("2E0402"))
+            self.borderColor = HTMLColor(light: .hex("D02C1E"), dark: .hex("EB4642"))
         case "Important":
-            self.backgroundColor = HTMLColor(light: .hex("#FEFBF3"), dark: .hex("#291F04"))
-            self.borderColor = HTMLColor(light: .hex("#966922"), dark: .hex("#F4B842"))
+            self.backgroundColor = HTMLColor(light: .hex("FEFBF3"), dark: .hex("291F04"))
+            self.borderColor = HTMLColor(light: .hex("966922"), dark: .hex("F4B842"))
         case "Announcement", "Tip":
-            self.backgroundColor = HTMLColor(light: .hex("#FBFFFF"), dark: .hex("#0F2C2B"))
-            self.borderColor = HTMLColor(light: .hex("#4B767C"), dark: .hex("#9FFCE5"))
+            self.backgroundColor = HTMLColor(light: .hex("FBFFFF"), dark: .hex("0F2C2B"))
+            self.borderColor = HTMLColor(light: .hex("4B767C"), dark: .hex("9FFCE5"))
         case "Preamble":
-            self.backgroundColor = HTMLColor(light: .hex("#FBF8FF"), dark: .hex("#1e1925"))
-            self.borderColor = HTMLColor(light: .hex("#8D51F6"), dark: .hex("#8D51F6"))
+            self.backgroundColor = HTMLColor(light: .hex("FBF8FF"), dark: .hex("1e1925"))
+            self.borderColor = HTMLColor(light: .hex("8D51F6"), dark: .hex("8D51F6"))
         default:
-            self.backgroundColor = HTMLColor(light: .hex("#f5f5f5"), dark: .hex("#323232"))
-            self.borderColor = HTMLColor(light: .hex("#696969"), dark: .hex("#9a9a9a"))
+            self.backgroundColor = HTMLColor(light: .hex("f5f5f5"), dark: .hex("323232"))
+            self.borderColor = HTMLColor(light: .hex("696969"), dark: .hex("9a9a9a"))
         }
     }
 }
