c407d74a0f07445e79436930e6ef6744313b94c1
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-09-15 00:00:21 +0200

    wip

--- Sources/HTML/Divider.swift
+++ Sources/HTML/Divider.swift
@@ -10,7 +10,7 @@ public struct Divider: HTML {
             .inlineStyle("border-bottom", "none")
             .inlineStyle("border-left", "none")
 //            .border(.top(width: 1.px, style: .solid, color: .gray800.dark(.gray300)))
-            .border(.top(width: 1.px, style: .solid, color: .gray550.dark(.gray450)))
+            .border(.top(width: 1.px, style: .solid, color: .gray800.dark(.gray200)))
             .margin(vertical: 0, horizontal: 30.percent)
     }
 }
