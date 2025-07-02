5ee9547ef5381ee817b4145df2a9f45b360bb369
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-02 09:45:51 +0200

    fix

--- Sources/HTML/Enhancements/Background.swift
+++ Sources/HTML/Enhancements/Background.swift
@@ -11,14 +11,14 @@ import HTML_CSS_PointFreeHTML
 extension HTML {
     public func backgroundColor(
         _ color: HTMLColor?,
-        media mediaQuery: MediaQuery? = nil,
+        media mediaQuery: CSSAtRuleTypes.Media? = nil,
         pre: String? = nil,
         pseudo: Pseudo? = nil
     )-> some HTML {
-        let lightStyle = inlineStyle("background", color?.light.description, media: mediaQuery, pre: pre, pseudo: pseudo)
+        let lightStyle = inlineStyle("background", color?.light.description, media: mediaQuery.map(\.rawValue).map(MediaQuery.init), pre: pre, pseudo: pseudo)
         
         if let darkColor = color?.dark {
-            return lightStyle.inlineStyle("background", darkColor.description, media: .dark, pre: pre, pseudo: pseudo)
+            return lightStyle.inlineStyle("background", darkColor.description, media: MediaQuery(rawValue: CSSAtRuleTypes.Media.dark.rawValue), pre: pre, pseudo: pseudo)
         } else {
             return lightStyle
         }
