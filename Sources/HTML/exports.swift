18bc9bd8f7f798bbb626eb83ba53e05b680588c1
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-22 07:32:52 +0200

    selective exports

--- Sources/HTML/exports.swift
+++ Sources/HTML/exports.swift
@@ -5,9 +5,4 @@
 //  Created by Coen ten Thije Boonkkamp on 01/09/2024.
 //
 
-// @_exported import PointFreeHTML
-@_exported import CSS_PointFreeHTML
-@_exported import CSSTypes
 @_exported import HTML_CSS_PointFreeHTML
-@_exported import HTMLAttributes_PointFreeHTML
-@_exported import HTMLElements_PointFreeHTML
